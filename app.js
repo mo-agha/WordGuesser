367e7f7d4bbdffb294e7bbcf894472829b30dbe3
mo-agha <73283975+mo-agha@users.noreply.github.com>
2023-11-24 13:02:58 -0800

    Updates
    Added interactive on-screen keyboard.
    New event listeners.
    Fixed edge case for repeated character in guessed word.
    Color animation continued, reflected on on-screen keyboard.
    Restyled page, grid, and components.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { generate } from 'https://cdn.jsdelivr.net/npm/random-words@2.0.0/+esm';
 const secretWord = generate({minLength : 5, maxLength : 5 });
 
 // WordsAPI API Key
-const API_KEY = 'df74eca4f4mshba688ef5a2abcb1p12376fjsnde18acc12f62';
+const API_KEY = config.MY_KEY;
 
 // Initialize the game state
 let attempts = 0;
@@ -17,22 +17,116 @@ const guessInput = document.getElementById("guess-input");
 const guessButton = document.getElementById("guess-button");
 const resultDisplay = document.getElementById("result");
 const testDisplay = document.getElementById("test");
+const keyButtons = Array.from(document.getElementsByClassName("keyboard-button"));
+
+// Function to get number of instances a letter appears in a word
+function getLetterInstances(letter, word){
+    let instances = 0;
+    for (let i = 0; i < word.length; i++) {
+        if(word[i] === letter){
+            instances++;
+        }
+    }
+    return instances;
+}
+
+// Function to get the position of a letter in a word
+function getLetterPosition(letter, word, index){
+    let position = 0;
+    for (let i = 0; i <= index; i++) {
+        if(word[i] === letter){
+            position++;
+        }
+    }
+    return position;
+}
+
+function clickKeyButton(button){
+    if(button.textContent === 'ENTER'){
+        handleGuess();
+    }
+    else if(button.textContent === 'DEL'){
+        if(guessInput.value !== null){
+            guessInput.value = guessInput.value.slice(0, -1);
+        }
+    }
+    else{
+        guessInput.value += button.textContent;
+    }
+    guessInput.focus();
+    
+}
+
+function colorKeyButton(letter, color) {
+    for (const element of document.getElementsByClassName('keyboard-button')) {
+        console.log(`Letter: ${letter}, Button Text: ${element.textContent}, Current Color: ${element.style.backgroundColor}, New Color: ${color}`);
+        if (element.textContent.toLowerCase() === letter) {
+            if (element.style.backgroundColor === 'forestgreen') {
+                return;
+            }
+            if (element.style.backgroundColor === 'mediumvioletred' && color !== 'forestgreen') {
+                return;
+            }
+            if (color === '#508d9c') {
+                if(element.style.backgroundColor === 'dimgray'){
+                    return;
+                } else {
+                    console.log(`Setting button color to grey.`);
+                    element.style.backgroundColor = 'dimgray';
+                    return;
+                }
+            }
+            console.log(`Matching button found.`);
+            console.log(`Before setting color - Current Color: ${element.style.backgroundColor}, New Color: ${color}`);
+            element.style.backgroundColor = color;
+            console.log(`After setting color - Current Color: ${element.style.backgroundColor}, New Color: ${color}`);
+            break;
+        }
+    }
+}
+
 
 // Function to update the word display
 function updateWordDisplay(rowIndex) {
     const letterRow = document.querySelectorAll('.letter-row')[rowIndex];
     const letterSquares = letterRow.querySelectorAll('.letter-square');
+    const animationDuration = 500;
 
     for (let i = 0; i < secretWord.length; i++) {
-        letterSquares[i].textContent = guessedWord[i];
-        if(guessedWord[i] === secretWord[i]){
-            letterSquares[i].style.backgroundColor = 'green';
+        let lettercolor = '';
+        let letter = guessedWord[i];
+        let box = letterSquares[i];
+        box.textContent = guessedWord[i];
+        box.classList.add('animated');
+        box.style.animationDelay = `${(i * animationDuration) / 2}ms`;
+
+        const letterInstancesSecret = getLetterInstances(box.textContent, secretWord);
+        const letterInstancesGuess = getLetterInstances(box.textContent, guessedWord);
+        const letterPosition = getLetterPosition(box.textContent, guessedWord, i);
+
+            if (letterInstancesGuess > letterInstancesSecret && letterPosition > letterInstancesSecret) {
+                //if(guessedWord[i] === secretWord[i]){     //Case of word with 2 letters. first in wrong position (purple), second in correct position (stays blue).
+                //    lettercolor = 'forestgreen';
+                //}
+                lettercolor = '#508d9c';
+            }
+            else if (guessedWord[i] === secretWord[i]) {
+                    lettercolor = 'forestgreen';
+            }
+            else if (secretWord.includes(guessedWord[i])) {
+                    lettercolor = 'mediumvioletred';
+            }
+            else{
+                lettercolor = '#508d9c';
+            }
+            setTimeout(() => {
+                box.style.backgroundColor = lettercolor;
+                colorKeyButton(letter, lettercolor);
+            }, (i+1) * animationDuration / 2);
         }
-        else if(secretWord.includes(guessedWord[i])){
-            letterSquares[i].style.backgroundColor = '#c034eb';
-        }
-    }
+
     rowIndex++;
+
 }
 
 // Function to set difficulty level and reset the game
@@ -70,16 +164,13 @@ function setDifficulty(difficulty) {
     }
 
     attempts = 0;
-    guessedWord = "_".repeat(secretWord.length);
     guessButton.disabled = false;
     resultDisplay.textContent = `Attempts: ${attempts}/${maxAttempts}`;
     testDisplay.textContent = 'Test word: ' + secretWord;
-    //updateWordDisplay();
 }
 
 // Function to handle a guess
 async function handleGuess() {
-    // let rowNumber = 0;
     const guess = guessInput.value.trim().toLowerCase(); // Trim whitespace and convert to lowercase
     const isValid = await checkWordValidity(guess);
 
@@ -103,12 +194,14 @@ async function handleGuess() {
     attempts++;
 
     updateWordDisplay(rowNumber);
-
+    
     if (guessedWord === secretWord) {
         resultDisplay.textContent = `Congratulations! You guessed the word "${secretWord}" in ${attempts} attempts.`;
+        resultDisplay.classList.add('win-animation');
         guessButton.disabled = true;
     } else if (attempts === maxAttempts) {
         resultDisplay.textContent = `Game over! The secret word was "${secretWord}".`;
+        resultDisplay.classList.add('loser-animation')
         guessButton.disabled = true;
     } else {
         rowNumber++;
@@ -170,6 +263,16 @@ document.getElementById("hard-button").addEventListener("click", function() {
 
 // Initialize the game with medium difficulty as the default
 setDifficulty("medium");
+guessInput.focus();
 
-// Event listener for the guess button
+// Event listeners for the guess button and input field
 guessButton.addEventListener("click", handleGuess);
+guessInput.addEventListener("keypress", function(e){
+    if(e.key === 'Enter'){
+        handleGuess();
+    }
+});
+
+for (const button of keyButtons){
+    button.addEventListener("click", () => clickKeyButton(button));
+}
